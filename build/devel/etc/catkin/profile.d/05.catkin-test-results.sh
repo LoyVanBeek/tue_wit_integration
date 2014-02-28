a749c826e0b22a4458e216ea88ee424bc4948809
Loy van Beek <loy.vanbeek@gmail.com>
2014-02-28 23:55:42 +0100

    Removed build dir. Thougt I already done so

--- build/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/loy/ros/groovy/repos/yol-ros-pkg/tue_wit_integration/build/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
