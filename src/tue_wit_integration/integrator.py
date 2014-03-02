2804a022f5576733a4f4a9e4e32ad8d141c81ab7
Loy van Beek <loy.vanbeek@gmail.com>
2014-03-02 11:25:13 +0100

    Made new interpretation for the move-intent in my Wit.ai-instance. I can say: amigo, go to the kitchen and the command is interpreted correctly :-D

--- src/tue_wit_integration/integrator.py
+++ src/tue_wit_integration/integrator.py
@@ -13,12 +13,14 @@ from wit_ros.msg import Outcome, Entity
 
 from intents.hello import Hello
 from intents.give import Give
+from intents.move import Move
 
 import robot_skills.amigo
 
 mapping = dict()
 mapping["hello"] = Hello
 mapping["give"] = Give
+mapping["move"] = Move
 
 class Integration(object):
     """Listen until called by name, then start wit_ros's listen_and_interpret service"""
@@ -26,24 +28,36 @@ class Integration(object):
         self.robot = robot
         self.listen_and_interpret = rospy.ServiceProxy("/wit/listen_interpret", ListenAndInterpret)
 
+        self.busy = False
+
     def check_for_calling(self, message):
         rospy.logdebug("Received text {0}".format(message.data))
-        if message.data.lower() in ["amigo", "sergio"]: #Susan because the others are not in the dorpus ATM
+        if message.data.lower() in ["amigo", "sergio"]:
             #I have been called!
-            try:
-                rospy.loginfo("Start listening...")
-                response = self.listen_and_interpret()
-                outcome = response.outcome
+            if self.busy: 
+                rospy.loginfo("Call heard, but busy processing earlier call")
+            if not self.busy:
+                self.busy = True
+                try:
+                    rospy.loginfo("Start listening...")
+                    response = self.listen_and_interpret()
+                    outcome = response.outcome
 
-                rospy.loginfo("Starting execution of {0}".format(outcome))
-                interpreting_class = mapping[outcome.intent] #Which class to use for this interpretation
-                interpreter = interpreting_class(self.robot) #Instantiate an instance of this class
-                interpreter.interpret(outcome) #Finally pass the outcome to this class
-            except rospy.ServiceException:
-                rospy.logerr("Could not interpret command")
+                    rospy.loginfo("Starting execution of {0}".format(outcome))
+                    try:
+                        interpreting_class = mapping[outcome.intent] #Which class to use for this interpretation
+                        interpreter = interpreting_class(self.robot) #Instantiate an instance of this class
+                        interpreter.interpret(outcome) #Finally pass the outcome to this class
+                    except KeyError:
+                        rospy.logerr("No interpretation for intent '{0}'".format(outcome.intent))
+                        self.robot.speech.speak("I heard what you said, but i don't know how to process a {0} command".format(outcome.intent.replace("_", " ")))
+                except rospy.ServiceException:
+                    rospy.logerr("Could not interpret command")
+                    self.robot.speech.speak("I couldn't hear you properly, maybe my ears are clogged")
+                self.busy = False
 
 if __name__ == "__main__":
-    rospy.init_node("tue_wit_integration", log_level=rospy.DEBUG)
+    rospy.init_node("tue_wit_integration", log_level=rospy.INFO)
 
     robot = robot_skills.amigo.Amigo(dontInclude=['base','arms','perception','head', 'worldmodel', 'reasoner'])
 
