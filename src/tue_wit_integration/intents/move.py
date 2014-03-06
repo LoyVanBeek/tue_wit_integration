3adc137ba2a4cb0686adf5fbd1d68dde5ef6ae52
Loy van Beek <loy.vanbeek@gmail.com>
2014-03-06 22:14:16 +0100

    Building proper queries now to see if the intended destination exists. Done by querying all waypoints and poi's to see if any name matches. I think it will have problems with numbered items. The Compound names are a nice way to separate name and index or sub-location

--- src/tue_wit_integration/intents/move.py
+++ src/tue_wit_integration/intents/move.py
@@ -3,10 +3,11 @@
 
 import roslib
 roslib.load_manifest('tue_wit_integration')
+import rospy
 
 import robot_smach_states as states
 
-from psi import Compound, Conjunction, Sequence
+from psi import Compound, Conjunction, Sequence, Constant
 
 class Move(object):
     def __init__(self, robot):
@@ -14,15 +15,46 @@ class Move(object):
         self.robot = robot
 
     def interpret(self, outcome):
-        #import ipdb; ipdb.set_trace()
+        import ipdb; ipdb.set_trace()
+
+        
+        self.robot.reasoner.query(Compound("load_database", "tue_knowledge", 'prolog/locations.pl'))
+        self.robot.reasoner.query(Compound("load_database", "tue_knowledge", 'prolog/objects.pl'))
 
         name = "nowhere"
         nav_state = True
         try:
             destination = [entity.value for entity in outcome.entities if "destination" in entity.name][0]
-            name = "the " + destination
-            q = Compound("point_of_interest", destination, Compound("point_3d", "X", "Y", "Z"))
-            #nav_state = states.NavigateGeneric(self.robot, lookat_query=q)
+
+            wp = Conjunction(Compound("environment", "Env"), Compound("waypoint", "Env", "Chal", "Name", "Pose"))
+            waypoints = self.robot.reasoner.query(wp)
+            simple_waypoint_names = [str(ans["Name"]).replace("_", " ") for ans in waypoints if isinstance(ans["Name"], Constant)]
+
+            kwargs = {}
+            if destination in simple_waypoint_names:
+                name = "the " + destination
+                query = Conjunction(
+                        Compound("environment", "E"), 
+                        Compound("waypoint", "E", "Challenge", destination, Compound("pose_2d", "X", "Y", "Phi")))
+                kwargs = {"goal_query":query}
+            else:
+                poi = Conjunction(Compound("environment", "Env"), Compound("point_of_interest", "Env", "Chal", "Name", "Point"))
+                pois = self.robot.reasoner.query(poi)
+                simple_poi_names = [str(ans["Name"]).replace("_", " ") for ans in pois if isinstance(ans["Name"], Constant)]
+                if destination in simple_poi_names:
+                    name = "the " + destination
+                    query = Conjunction(
+                            Compound("environment", "E"), 
+                            Compound("point_of_interest", destination, Compound("point_3d", "X", "Y", "Z")))
+                    kwargs = {"lookat_query":query}
+
+                self.robot.speach.speak("There is nothing named {0}, I don't know where to go".format(destination))
+
+            try:
+                nav_state = states.NavigateGeneric(self.robot, **kwargs)
+            except Exception, e:
+                rospy.logerr(e)
+
         except:
             pass
 
@@ -32,7 +64,10 @@ class Move(object):
             q = Conjunction(  Compound("property_expected", "ObjectID", "class_label", "person"),
                               Compound("property_expected", "ObjectID", "position", Sequence("X","Y","Z")),
                               Compound("property_expected", "ObjectID", "name", destination))
-            #nav_state = states.NavigateGeneric(self.robot, lookat_query=q)
+            try:
+                nav_state = states.NavigateGeneric(self.robot, lookat_query=q)
+            except Exception, e:
+                rospy.logerr(e)
         except:
             pass
 
